807b44b587c8d64a01bbacd746ff4dcdab7df668
vj-codes <vj.codes29@gmail.com>
2021-07-04 00:28:48 +0530

    Updates

---
+++ precache-manifest.ba5a9f477f92e25dffcf208684ed75e5.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b08f86d78ba98f5aceef2c07cc797f",
+    "url": "vj-codes.github.io/index.html"
+  },
+  {
+    "revision": "9273e3c2dde77adad956",
+    "url": "vj-codes.github.io/static/css/main.3530bcfd.chunk.css"
+  },
+  {
+    "revision": "34d6cebb95c5c5594b5a",
+    "url": "vj-codes.github.io/static/js/2.581d2dfc.chunk.js"
+  },
+  {
+    "revision": "9273e3c2dde77adad956",
+    "url": "vj-codes.github.io/static/js/main.da954685.chunk.js"
+  },
+  {
+    "revision": "752cc3ede164e82202aa",
+    "url": "vj-codes.github.io/static/js/runtime-main.9763c4cd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "vj-codes.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "vj-codes.github.io/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "vj-codes.github.io/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "vj-codes.github.io/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "vj-codes.github.io/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "vj-codes.github.io/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "vj-codes.github.io/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "b175cf581b37b9df2300f23ab3996748",
+    "url": "vj-codes.github.io/static/media/IntroHome.b175cf58.svg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "vj-codes.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "05212119e95e2c6d7f1e6c1ead306b0a",
+    "url": "vj-codes.github.io/static/media/anitab_logo.05212119.png"
+  },
+  {
+    "revision": "fbb4a7f9332a3aa0d792a6221e948ff0",
+    "url": "vj-codes.github.io/static/media/cisce-logo.fbb4a7f9.png"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "vj-codes.github.io/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "9ac602258f1e8981aa70f226d3e188dc",
+    "url": "vj-codes.github.io/static/media/coderImg.9ac60225.svg"
+  },
+  {
+    "revision": "9fde48d05f100050055cca54ae231b43",
+    "url": "vj-codes.github.io/static/media/education.9fde48d0.svg"
+  },
+  {
+    "revision": "64f28575167c5aaf42011624c6177d4b",
+    "url": "vj-codes.github.io/static/media/experience.64f28575.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "vj-codes.github.io/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "vj-codes.github.io/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "vj-codes.github.io/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "vj-codes.github.io/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "vj-codes.github.io/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "vj-codes.github.io/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "vj-codes.github.io/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "vj-codes.github.io/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "vj-codes.github.io/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "vj-codes.github.io/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "vj-codes.github.io/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "vj-codes.github.io/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "vj-codes.github.io/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "vj-codes.github.io/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "vj-codes.github.io/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "192c18c13f17d8970381450479e75340",
+    "url": "vj-codes.github.io/static/media/fb-logo.192c18c1.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "vj-codes.github.io/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "b5c36e6563d154c0d11a7c488e173d14",
+    "url": "vj-codes.github.io/static/media/hsc-logo.b5c36e65.png"
+  },
+  {
+    "revision": "dc5b7aad8ac6c3ec98c15d5ca6ceb9d5",
+    "url": "vj-codes.github.io/static/media/icse_logo.dc5b7aad.jpg"
+  },
+  {
+    "revision": "2f377df20619d1976aa9556af6a23bbe",
+    "url": "vj-codes.github.io/static/media/iitb_logo.2f377df2.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "vj-codes.github.io/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "vj-codes.github.io/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "d0f8d2a41ce287ad63a5b2eff2777f20",
+    "url": "vj-codes.github.io/static/media/profile.d0f8d2a4.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "vj-codes.github.io/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
